44ac5857dbd09d895a896466f13b7fade9f5cc49
= <=>
2021-08-30 20:33:07 +0200

    Dodanie projektu wraz ze struktura oraz wstepnym configiem

---
+++ src/main/java/io/github/s19151/config/AppConfig.java
@@ -0,0 +1,21 @@
+package io.github.s19151.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("io.github.s19151")
+public class AppConfig {
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		
+		vr.setPrefix("/WEB-INF/");
+		vr.setSuffix(".jsp");
+		
+		return vr;
+	}
+}
